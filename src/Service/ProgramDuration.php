0a42e0219f90105ecd961738958dee0311fa2877
BENJAMIN-SAUSSAYE <saussaye.ipod@gmail>
2023-05-29 14:22:53 +0200

    fix calculate duration function

--- src/Service/ProgramDuration.php
+++ src/Service/ProgramDuration.php
@@ -15,7 +15,22 @@ class ProgramDuration
 				$totalMinutes += $episode->getDuration();
 			}
 		}
-		$totalHours = ($totalMinutes / 60);
-		return strval($totalHours);
+		return $this->convertMinuteToDaysHours($totalMinutes);
+	}
+
+	private function convertMinuteToDaysHours(int $numberOfMinutes): string
+	{
+		if ($numberOfMinutes < 60) {
+			return sprintf("%d mins", $numberOfMinutes);
+		} else if ($numberOfMinutes % 60 == 0) { // No minutes - i.e. not a fractional hour.
+			return sprintf("%d hrs", $numberOfMinutes / 60);
+		} else if ($numberOfMinutes < 1440) { //1 day = 1440 minutes
+			return sprintf("%d hrs, %d mins", $numberOfMinutes / 60, $numberOfMinutes % 60);
+		} else {
+			$days = (int)floor($numberOfMinutes / 1440);
+			$hours = (int)floor($numberOfMinutes % 1440 / 60);
+			$minutes = (int)floor(($numberOfMinutes % 1440 % 60) % 60);
+			return sprintf("%d days, %d hrs, %d mins", $days, $hours, $minutes);
+		}
 	}
 }
